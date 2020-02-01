683a985b624a83d697b24566302529460117ea66
Alan Potter <alanjpotter@gmail.com>
2020-02-01 16:19:16 -0500

    Fix rake task loading for Rails 6
    Autoloading in Rails 6 (and maybe other versions) causes the symbol
    `Rake` to be defined when running the `rails` command. Change the check
    in `lib/slosilo.rb` to look for `Rake::DSL` instead.

--- lib/slosilo.rb
+++ lib/slosilo.rb
@@ -10,4 +10,8 @@ if defined? Sequel
   require 'slosilo/adapters/sequel_adapter'
   Slosilo::adapter = Slosilo::Adapters::SequelAdapter.new
 end
-Dir[File.join(File.dirname(__FILE__), 'tasks/*.rake')].each { |ext| load ext } if defined?(Rake)
+
+# Autoloading in Rails 6 (and maybe other version) means `Rake` will
+# be defined when we're loaded by the `rails` command. `Rake::DSL`
+# doesn't get defined unless we're actually run by `rake`.
+Dir[File.join(File.dirname(__FILE__), 'tasks/*.rake')].each { |ext| load ext } if defined?(Rake::DSL)
